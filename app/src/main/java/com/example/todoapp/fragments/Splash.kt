120a13b658112aee6c504bf7ec2dc02d2de31f20
nehapatilMF <nehapatil.kh@gmail.com>
2023-09-20 15:03:24 +0530

    all working

--- app/src/main/java/com/example/todoapp/fragments/Splash.kt
+++ app/src/main/java/com/example/todoapp/fragments/Splash.kt
@@ -2,21 +2,17 @@ package com.example.todoapp.fragments
 
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.todoapp.Constants
 import com.example.todoapp.R
 import com.example.todoapp.client.SessionManager
 import com.example.todoapp.databinding.FragmentSplashBinding
-import com.example.todoapp.viewModels.RefreshTokenViewModel
 
 @Suppress("DEPRECATION")
 class Splash : Fragment() {
@@ -42,10 +38,14 @@ class Splash : Fragment() {
             }
             override fun onAnimationEnd(animation: Animation?) {
                 val sessionManager = SessionManager(requireContext())
-                if (sessionManager.isLoggedIn() && sessionManager.isAccessTokenExpired()) {
-                    refreshToken()
+                if (sessionManager.getAccessToken()?.isNotBlank() == true) {
+                    findNavController().navigate(R.id.navigate_to_home)
+
+                    Constants.accessToken = sessionManager.getAccessToken().toString()
                 } else {
-                    proceedToNextScreen()
+                    findNavController().navigate(R.id.navigate_to_intro)
+                    Constants.clearAccessToken()
+                    sessionManager.clearTokens()
                 }
             }
             override fun onAnimationRepeat(animation: Animation?) {
@@ -55,48 +55,6 @@ class Splash : Fragment() {
         return binding?.root
     }
 
-    private fun refreshToken() {
-        val handler = Handler()
-        handler.postDelayed({
-        val viewModel = ViewModelProvider(this)[RefreshTokenViewModel::class.java]
-          val refreshToken = SessionManager(requireContext()).getRefreshToken().toString()
 
-          viewModel.refreshToken(refreshToken)
-        val sessionManager = SessionManager(requireContext())
-        viewModel.result.observe(viewLifecycleOwner){ status ->
-            if(status == "200"){
-                sessionManager.clearTokens()
-                Constants.clearAccessToken()
-                viewModel.getAuthTokens().observe(viewLifecycleOwner){ authTokens ->
-                    val accessToken = authTokens.accessToken
-                    Constants.accessToken = accessToken
-                    val refreshToken2 = authTokens.refreshToken
-                    Constants.refreshToken = refreshToken2
-                    sessionManager.saveTokens(accessToken,refreshToken2)
-                }
-            }
-            else{
-                Toast.makeText(requireContext(),status.toString(),Toast.LENGTH_SHORT).show()
-            }
-        }
-            proceedToNextScreen()
-        }, 2000) // Simulating a delay for the token refresh request
-    }
 
-    private fun proceedToNextScreen() {
-        if (SessionManager(requireContext()).isLoggedIn()) {
-            navigateToMainScreen()
-        } else {
-            navigateToLoginScreen()
-        }
     }
-
-    private fun navigateToMainScreen() {
-        findNavController().navigate(R.id.navigate_to_home)
-        Constants.accessToken = SessionManager(requireContext()).getAccessToken()
-    }
-
-    private fun navigateToLoginScreen() {
-        findNavController().navigate(R.id.navigate_to_intro)
-    }
-}
